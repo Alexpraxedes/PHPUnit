f73c06548c67e89586734347d710269b34cb3d74
Allisson Rafael de Brito Araujo <allissonads@outlook.com>
2022-09-13 18:42:10 -0300

    #002
    Simulando erro na exception nome pessoa

--- tests/PessoaTest.php
+++ tests/PessoaTest.php
@@ -9,7 +9,7 @@ class PessoaTest extends TestCase{
     {
         $pessoa = new Pessoa;
         $this->expectOutputString('João');
-        $pessoa->nome('João');
+        $pessoa->nome('Maria');
     }
 
 }
